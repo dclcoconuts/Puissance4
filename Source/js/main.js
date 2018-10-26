5551a40a9aa33745ad3a92067339d9ec90e6cad4
Sylvie <dcl.sylvieg@18pixel.fr>
2018-10-26 16:09:31 +0200

    debut HTML CSS JS

---
+++ Source/js/main.js
@@ -0,0 +1,18 @@
+
+var choixCol1 = document.getElementById("imgCol1");
+var choixCol2 = document.getElementById("imgCol2");
+var choixCol3 = document.getElementById("imgCol3");
+var choixCol4 = document.getElementById("imgCol4");
+var choixCol5 = document.getElementById("imgCol5");
+var choixCol6 = document.getElementById("imgCol6");
+var choixCol7 = document.getElementById("imgCol7");
+
+choixCol1.src = "img/Avatar_Coconut_mini.png";
+// choixCol2.src = "img/Avatar_Licorne_mini.png";
+// choixCol1.setAttribute("src", "img/Avatar_Coconut_mini.png");
+choixCol2.setAttribute("src", "img/Avatar_Licorne_mini.png");
+
+// document.getElementById("col1").src = "img/Avatar_Coconut_mini.png";
+
+var url1 = "img/Avatar_Licorne_mini.png";
+document.getElementById("case2").style.backgroundImage = "url('" + url1 + "')";
