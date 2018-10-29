28001334236add27dfd49dfe3745b9af6e4a1ba2
Sylvie <dcl.sylvieg@18pixel.fr>
2018-10-29 13:46:27 +0100

    positionnement du pion dans la grille après clic dans zone choix col

--- Source/js/main.js
+++ Source/js/main.js
@@ -1,11 +1,11 @@
 
-var choixCol1 = document.getElementById("imgCol1");
-var choixCol2 = document.getElementById("imgCol2");
-var choixCol3 = document.getElementById("imgCol3");
-var choixCol4 = document.getElementById("imgCol4");
-var choixCol5 = document.getElementById("imgCol5");
-var choixCol6 = document.getElementById("imgCol6");
-var choixCol7 = document.getElementById("imgCol7");
+// var choixCol1 = document.getElementById("y0");
+// var choixCol2 = document.getElementById("y1");
+// var choixCol3 = document.getElementById("y2");
+// var choixCol4 = document.getElementById("y3");
+// var choixCol5 = document.getElementById("y4");
+// var choixCol6 = document.getElementById("y5");
+// var choixCol7 = document.getElementById("y6");
 
 
 // ESSAIS :
@@ -21,9 +21,15 @@ var choixCol7 = document.getElementById("imgCol7");
 
 
 // retrouver la correspondance d'une case (id) au click :
-choixCol2.addEventListener("click", function(e){
-    console.log(event.target.id);   // affiche imgCol2
-});
+// choixCol2.addEventListener("click", function(e){
+//     console.log(event.target.id);   // affiche imgCol2
+// });
+
+// choixCol2.addEventListener("click", function(){
+//    // affichage du logo dans la seconde case de la grille :
+//     var url1 = "img/Avatar_Licorne_mini.png";
+//     document.getElementById("case2").style.backgroundImage = "url('" + url1 + "')";
+// });
 
 
 // création d'un tableau avec 7 lignes(i) et 7 colonnes(j)
@@ -42,17 +48,54 @@ for (var i=0; i<7; i++){
 for (var i = 0; i < grille.length; i++){
     console.log(grille[i]);
 }
- 
-
-// choixCol2.addEventListener("click", function(){
-//    // affichage du logo dans la seconde case de la grille :
-//     var url1 = "img/Avatar_Licorne_mini.png";
-//     document.getElementById("case2").style.backgroundImage = "url('" + url1 + "')";
 
 
-// });
+
+// ---------------------------------------------------------------------
 
 
- 
+// déclaration des variables
+var coordY;   // coordonnee Y
+var coordX;   // coordonnee X
+var joueur1 = 1;
+var joueur2 = 2;
+var joueurEnCours = 1;  //  en attendant 
+var idCase;     //id de la case
+var urlJeton = "img/Avatar_Licorne_mini.png";  // en attendant
+
+
+// zone de clic jeu
+var choixCol = document.getElementById("choixColonne");
+
+// fonction recherche coordonnee X
+function rechX (){
+    for (coordX=6; coordX >=0; coordX--){
+        if (grille[coordX] [coordY] == 0) {
+            grille[coordX] [coordY] = joueurEnCours;
+            break;
+        }
+    }
+    return coordX;
+};
+
+//fonction affichage jeton dans la case
+function jetonCase (urlJeton, idCase) {
+    document.getElementById(idCase).style.backgroundImage = "url('" + urlJeton + "')";
+};
+
+// récupération de la coordonnée Y
+choixCol.addEventListener("click", function(event){
+    var colChoisie =event.target.id; //enregistre l'id de la cellule cliquee sous forme de string.
+    coordY = parseInt(colChoisie[1]);   // recupere le deuxième caractère de l' ID = chiffre
+    rechX();
+    idCase = "x" + coordX + '-y' + coordY;
+    jetonCase(urlJeton, idCase);
+    console.log(grille);
+
+
+}); 
+
+
+
 
 
